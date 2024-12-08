4defc1ffd9ad67af2b6597c1e17bb0dd63fcd9d8
Mohamed Medhat <momd7at4@gmail.com>
2024-12-08 10:10:01 +0200

    explain card 3

--- Card3.cpp
+++ Card3.cpp
@@ -8,18 +8,19 @@ Card3::Card3(const CellPosition& pos):Card(pos)
 
 void Card3::Apply(Grid* pGrid, Player* pPlayer)
 {
-	Cell *x = pPlayer->GetCell();
+	Cell *x = pPlayer->GetCell(); // to get the cell which player at
 	const CellPosition cellposition = x->GetCellPosition();
-	Card::Apply(pGrid, pPlayer);
-	Ladder *l = pGrid->GetNextLadder(cellposition);
-	const CellPosition new_cellposition = ((GameObject*)l)->GetPosition();
-	int startcell = new_cellposition.GetCellNum();
+	Card::Apply(pGrid, pPlayer); // like given card 1
+	Ladder *l = pGrid->GetNextLadder(cellposition); // to get the next ladder after the current cell which player at
+	
 	if (pGrid == NULL) // I am not sure if i put l is correct or not 
 	{
 
 	}
 	else
 	{
-		pPlayer->Move(pGrid,startcell);
+		const CellPosition new_cellposition = ((GameObject*)l)->GetPosition(); // to get the new cell poaition which have the start of ladder
+		int startcell = new_cellposition.GetCellNum(); // to get the cell number of the new cell position
+		pPlayer->Move(pGrid,startcell); // move the player to the new cell number
 	}
 }
