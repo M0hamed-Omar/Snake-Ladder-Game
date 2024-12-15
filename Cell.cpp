950206b719d6c7d2f8535cf5563f3318da9f8ffe
Mohamed Medhat <momd7at4@gmail.com>
2024-12-15 11:06:08 +0200

    d

--- Cell.cpp
+++ Cell.cpp
@@ -33,7 +33,8 @@ bool Cell::SetGameObject(GameObject * pGObj)
 {
 	if (pGameObject != NULL && pGObj != NULL) // already contains one
 		return false; // do NOT add it and return false
-
+	if (pGameObject != NULL)
+		delete pGameObject;
 	pGameObject = pGObj;
 	return true;
 }
