a0163a653da0442be5e16ec2182785832834a7d3
Karim <karimmohamedmahmoud40@gmail.com>
2024-12-23 21:20:07 +0200

    card 5 fix

--- Card5.cpp
+++ Card5.cpp
@@ -12,7 +12,7 @@ void Card5::Apply(Grid* pGrid, Player* pPlayer)
 	int RollDiceNum = pPlayer->GetJustRolledDiceNum();
 	pPlayer->Move(pGrid, RollDiceNum);
 	Cell* ptr = pPlayer->GetCell();
-	if (ptr->HasLadder() || ptr->HasSnake())
+	if (ptr->GetGameObject())
 	{
 		ptr->GetGameObject()->Apply(pGrid,pPlayer);
 	}
